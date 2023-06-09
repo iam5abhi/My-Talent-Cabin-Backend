1e696ff1b76b60366785c66c865bf246b7c2500b
iam5abhi <support@codesoftic.com>
2023-06-09 11:54:29 +0530

    sdf

--- Controllers/usercontroller/usercontroller.js
+++ Controllers/usercontroller/usercontroller.js
@@ -7,8 +7,6 @@ const { REGISTRATION_SUCCESS, PASSWORD_NOT_MATCH, COMPARE_PASSWORD_USING_DB, LOG
 
 exports.signup = async (req, res, next) => {
     try {
-        // const errors = validationResult(req);
-        // if (!errors.isEmpty()) return res.status(422).send({ errors: errors.array() });
         const { name, email, password, confirmPassword,PhoneNumber} = req.body;
         if (base64.decode(password) !== base64.decode(confirmPassword)) return next(new Error(PASSWORD_NOT_MATCH, 400));
         const existingAdmin = await User.findOne({ email });
